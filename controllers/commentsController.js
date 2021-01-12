93cb82867d5b5042811162db1ee7755233bdcdff
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-12 16:01:31 -0500

    created create method for commentsController

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -7,5 +7,11 @@ module.exports = {
         .find(req.query)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.Comment
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
 }
