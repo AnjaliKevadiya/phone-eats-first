6e5821d9631f663af14482de155e4ca80ce6bca0
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-12 15:53:30 -0500

    created update method for the postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,8 +15,15 @@ module.exports = {
             .catch(err => res.status(422).json(err)); 
     },
     create: function (req, res) {
-        db.Post.create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+        db.Post
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Post
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 }
