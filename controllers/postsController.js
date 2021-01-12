8460774553b833377a031fe0cf3e85339c94e60c
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-12 15:56:40 -0500

    created remove method for the postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -25,5 +25,12 @@ module.exports = {
             .findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Post
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
-}
+};
