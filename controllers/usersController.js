bcc156df17ad1cc3d4ffd6b7ac6e530b10ebdc7d
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-12 15:46:56 -0500

    create method to find users by id in usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,4 +9,9 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  findById: function (req, res) {
+    db.User.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
