a0adfe2914f5052d807bde38c73c9b3b53aa34ba
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-12 15:48:30 -0500

    defined findById method for the postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,5 +7,11 @@ module.exports = {
             .find(req.query)
             .then(dbModel => res.json(dbModel))
             .catch (err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Post
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err)); 
     }
 }
