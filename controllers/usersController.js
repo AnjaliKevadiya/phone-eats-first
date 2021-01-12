b89ebde150728d552a7cbb8d8caf9014fe09a198
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-12 15:48:13 -0500

    created method to create new user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,4 +14,9 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  create: function (req, res) {
+    db.User.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
