0cf7eea1d6c77c6cfdee220f52b6209c630f476d
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-12 15:45:02 -0500

    create method to find all the users in usersController

---
+++ controllers/usersController.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+
+// Defining methods for the usersController
+
+module.exports = {
+  findAll: function (req, res) {
+    db.User.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
