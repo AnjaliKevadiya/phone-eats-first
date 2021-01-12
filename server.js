932cf75069d865ed214c4dcdd12d670f58c89abc
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-12 13:47:28 -0500

    defined middleware

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
