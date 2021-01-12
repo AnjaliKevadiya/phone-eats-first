794ed9b4d0b5009315e910cda528da6fcdb31ba8
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-12 15:50:18 -0500

    created method to update existing user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,4 +19,9 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  update: function (req, res) {
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
