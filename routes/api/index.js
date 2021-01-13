371cc29a38bf6f1ec48cd5d9ed63c6b8f745b54f
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-13 14:04:51 -0500

    updated seedDb.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,12 +4,12 @@ const postRoutes = require("./posts");
 const commentRoutes = require("./comments");
 
 // User routes
-router.use("./users", userRoutes);
+router.use("/users", userRoutes);
 
 // Post routes
-router.use("./posts", postRoutes);
+router.use("/posts", postRoutes);
 
 // Comment routes
-router.use("./comments", commentRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,78 +3,62 @@ const db = require("../models");
 
 //This file empties the Post
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/phone-eats-first");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/phone-eats-first"
+);
 
 const userSeed = [
-    {
-        email: "arthur.hoffman@example.com",
-        first_name: "Arthur",
-        last_name: "Hoffman",
-        profile_url: "",
-        posts: [
-            {
-                image: "https://images.app.goo.gl/qNpdxJGiJUHo9kJCA",
-                restaurant_name: "Chipotle",
-                rating: 5,
-                caption: "chicken burrito bowl",
-                number_of_likes: 10,
-                comments: [
-                    {
-                        username: "Cody Collins",
-                        comment: "Delicious!"
-                    }
-                ],
-            }
-        ]
-    },
-    {
-        email: "cody.collins@example.com",
-        first_name: "Cody",
-        last_name: "Collins",
-        profile_url: "",
-        posts: [
-            {
-                image: "https://images.app.goo.gl/QmLhidh65s97g2b49",
-                restaurant_name: "Sushi Palace",
-                rating: 5,
-                caption: "sushi rolls",
-                number_of_likes: 100,
-                comments: [
-                    {
-                        username: "Debbie Jensen",
-                        comment: "Yumm!!"
-                    }
-                ],
-            }
-        ]
-    },
-    {
-        email: "deanna.morris@example.com",
-        first_name: "Deanna",
-        last_name: "Morris",
-        profile_url: "",
-    },
-    {
-        email: "debbie.jensen@example.com",
-        first_name: "Debbie",
-        last_name: "Jensen",
-        profile_url: "",
-    },
-    {
-        email: "dwayne.gardner@example.com",
-        first_name: "Dwayne",
-        last_name: "Gardner",
-        profile_url: "",
-    }
+  {
+    email: "arthur.hoffman@example.com",
+    first_name: "Arthur",
+    last_name: "Hoffman",
+    profile_url: "",
+    posts: [
+      {
+        image: "https://images.app.goo.gl/qNpdxJGiJUHo9kJCA",
+        restaurant_name: "Chipotle",
+        rating: 5,
+        caption: "chicken burrito bowl",
+        number_of_likes: 10,
+        comments: [
+          {
+            username: "Cody Collins",
+            comment: "Delicious!",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    email: "cody.collins@example.com",
+    first_name: "Cody",
+    last_name: "Collins",
+    profile_url: "",
+    posts: [
+      {
+        image: "https://images.app.goo.gl/QmLhidh65s97g2b49",
+        restaurant_name: "Sushi Palace",
+        rating: 5,
+        caption: "sushi rolls",
+        number_of_likes: 100,
+        comments: [
+          {
+            username: "Debbie Jensen",
+            comment: "Yumm!!",
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 db.User.remove({})
-    .then(() => db.User.collection.insertMany(userSeed))
-    .then(data => {
-        console.log(data.result.n + " records inserted!");
-        process.exit(0);
-    })
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
