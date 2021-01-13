754fda2d440cecafabe7f04728ac3650a842b26f
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-13 11:01:39 -0500

    created route that  matches with /api/posts/:id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,4 +6,11 @@ router.route("/")
     .get(postsController.findAll)
     .post(booksController.create);
 
-    
+//Matches with "/api/posts/:id"
+router
+    .route("/:id")
+    .get(postsController.findById)
+    .put(postsController.update)
+    .delete(postsController.remove);
+
+module.exports = router;
