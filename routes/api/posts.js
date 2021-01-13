1979d215e69d759d4ebe84a6ce37504a558f8b0f
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-13 10:58:41 -0500

    created route that  matches with /api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,3 +2,8 @@ const router = require("express").Router();
 const postsController = require("../../controllers/postsController");
 
 //Matches with "/api/posts"
+router.route("/")
+    .get(postsController.findAll)
+    .post(booksController.create);
+
+    
