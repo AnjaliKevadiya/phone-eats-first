141eef91dc8f8f534f549b9830b648881bf0eece
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-13 15:19:57 -0500

    also added login method to the api user api route file

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
 // Matches with "/api/user"
-router.route("/").post(usersController.register);
-
-// router.route("/").get(usersController.findAll).post(usersController.register);
+router
+  .route("/")
+  .get(usersController.findAll)
+  .post(usersController.register)
+  .post(usersController.login);
 
 //Matches with "/api/user/:id"
 router.route("/:id").get(usersController.findById).put(usersController.update);
