2949375013faa258452a1fedec3a63a678614831
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-13 10:58:47 -0500

    export router in users api routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,3 +6,5 @@ router.route("/").get(usersController.findAll).post(usersController.create);
 
 //Matches with "/api/users/:id"
 router.route("/:id").get(usersController.findById).put(usersController.update);
+
+module.exports = router;
