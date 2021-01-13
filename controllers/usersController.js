de62a868d4f4dc176405368d50017fd1092e43e7
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-13 14:28:38 -0500

    implemented user registration method to create a new user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const bcrypt = require("bcrypt");
 
 // Defining methods for the usersController
 
@@ -14,10 +15,23 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  create: function (req, res) {
-    db.User.create(req.body)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+  register: async function (req, res) {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      console.log("hashed password", hashedPassword);
+      db.User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: hashedPassword,
+      }).then((userData) => {
+        console.log("registerd successfully", userData);
+        res.send({ user: userData.id, message: "Welcome!" });
+      });
+    } catch (err) {
+      res.status(422).json(err);
+    }
+    // db.User.create(req.body).then((dbModel) => res.json(dbModel));
   },
   update: function (req, res) {
     db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
