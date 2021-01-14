f57555e8a5126386d99cf2c6cca98551d4d4548d
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-14 14:30:30 -0500

    push

--- client/src/Components/SignUpForm/index.js
+++ client/src/Components/SignUpForm/index.js
@@ -1,62 +1,9 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 
 const SignUpForm = () => {
     return (
-        <MDBContainer>
-            <MDBRow>
-                <MDBCol md="6">
-                    <MDBCard>
-                        <MDBCardBody>
-                            <form>
-                                <p className="h4 text-center py-4">Sign up</p>
-                                <div className="grey-text">
-                                    <MDBInput
-                                        label="First Name"
-                                        icon="firstName"
-                                        group
-                                        type="text"
-                                        validate
-                                        error="wrong"
-                                        success="right"
-                                    />
-                                    <MDBInput
-                                        label="Last Name"
-                                        icon="lastName"
-                                        group
-                                        type="text"
-                                        validate
-                                        error="wrong"
-                                        success="right"
-                                    />
-                                    <MDBInput
-                                        label="Your email"
-                                        icon="envelope"
-                                        group
-                                        type="email"
-                                        validate
-                                        error="wrong"
-                                        success="right"
-                                    />
-                                    <MDBInput
-                                        label="Your password"
-                                        icon="lock"
-                                        group
-                                        type="password"
-                                        validate
-                                    />
-                                </div>
-                                <div className="text-center py-4 mt-3">
-                                    <MDBBtn color="cyan" type="submit">
-                                        Register
-                                    </MDBBtn>
-                                </div>
-                            </form>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+        <div></div>
+
     );
 };
 
