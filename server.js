b33706fee2a4a70683aae885921cce127eb61e2c
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-14 10:16:56 -0500

    added express session and presetup for passport in server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +16,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 app.use(routes);
