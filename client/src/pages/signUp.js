524d95ab5b06c19fe63c819af5f2cc30f7c28a91
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-14 10:52:48 -0500

    created the signup page

---
+++ client/src/pages/signUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "../Components/SignUp";
+
+function SignUp() {
+    return (
+        <div>
+            <SignUp />
+        </div>
+    )
+}
+
+export default SignUp;
