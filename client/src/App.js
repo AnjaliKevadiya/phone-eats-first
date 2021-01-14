ce1644bf8c797f7e17dcc6b0e2d334ff0997a995
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-14 12:20:55 -0500

    imported mdbreact

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Form from "./Components/Form";
-import Navbar from "./Components/Navbar";
 import SignUp from "./pages/SignUp"
-// import { StoreProvider } from "./utils/GlobalState";
 
 function App() {
   return (
     <Router>
-      {/* <div className="App">
-        <Navbar />
-        <Form />
-      </div> */}
       <div>
         <Switch>
-        {/* <StoreProvider> */}
           <Route exact path="/signup" component={SignUp} />
-        {/* </StoreProvider> */}
         </Switch>
             </div>
-
     </Router>
 
   );
