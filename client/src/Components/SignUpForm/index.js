1d1dd023b4fa016ece88e81924ebffafd2828bda
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-14 21:47:56 -0500

    created new sign up form

--- client/src/Components/SignUpForm/index.js
+++ client/src/Components/SignUpForm/index.js
@@ -1,8 +1,31 @@
 import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SignUpForm = () => {
     return (
-        <div></div>
+        <div className="row">
+        <form className="col s12">
+            <div className="row">
+                <div className="input-field col s6">
+                    <input placeholder="First Name" id="first_name" type="text" class="validate" />
+                </div>
+                <div className="input-field col s6">
+                    <input placeholder="Last Name" id="last_name" type="text" class="validate" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="input-field col s12">
+                    <input placeholder="password" id="password" type="password" class="validate" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="input-field col s12">
+                    <input placeholder="Email Address" id="email" type="email" class="validate" />
+                </div>
+            </div>
+        </form>
+    </div>
 
     );
 };
