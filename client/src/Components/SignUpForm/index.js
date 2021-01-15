13bded0827907269910c2f8c803b56ae021d6bcd
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-14 23:25:59 -0500

    styled sign up form

--- client/src/Components/SignUpForm/index.js
+++ client/src/Components/SignUpForm/index.js
@@ -4,29 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SignUpForm = () => {
     return (
-        <div className="row">
-        <form className="col s12">
+        <div className="container">
             <div className="row">
-                <div className="input-field col s6">
-                    <input placeholder="First Name" id="first_name" type="text" class="validate" />
-                </div>
-                <div className="input-field col s6">
-                    <input placeholder="Last Name" id="last_name" type="text" class="validate" />
-                </div>
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input placeholder="First Name" id="first_name" type="text" />
+                        </div>
+                        <div className="input-field col s6">
+                            <input placeholder="Last Name" id="last_name" type="text" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input placeholder="Email Address" id="email" type="email" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input placeholder="password" id="password" type="password" />
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <input placeholder="password" id="password" type="password" class="validate" />
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <input placeholder="Email Address" id="email" type="email" class="validate" />
-                </div>
-            </div>
-        </form>
-    </div>
-
+            <button type="submit" className="btn btn-outline-success">Sign Up</button>
+        </div>
     );
 };
 
