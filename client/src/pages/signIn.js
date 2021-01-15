89812d0f816c5490f814824bfc0b8fb15da475a6
joedip16 <joedippolito16@gmail.com>
2021-01-15 14:27:37 -0500

    completed sign in page

--- client/src/pages/signIn.js
+++ client/src/pages/signIn.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { Container, Row } from "../components/SignUpForm";
+import API from "../utils/API";
 function SignIn() {
-  const [username, setEmail] = useState();
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("username is " + username);
+    console.log("email is " + email);
     console.log("password is " + password);
+    API.login({
+      email: email,
+      password: password,
+    })
+      .then((res) => {
+        console.log("login response ", res);
+        window.location.href = "/home";
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div>
       <div className="mt-4">
-        <h2>Sign Up</h2>
+        <h2>Sign In</h2>
       </div>
       <form onSubmit={handleSubmit}>
         <Container className="mt-3 px-5">
@@ -36,12 +46,6 @@ function SignIn() {
           <button className="btn btn-success" type="submit">
             Submit
           </button>
-        </Container>
-        <Container className="mt-4">
-          <h3>Hello {username}!</h3>
-          <p>
-            I probably shouldn't tell you this, but your password is {password}!
-          </p>
         </Container>
       </form>
     </div>
