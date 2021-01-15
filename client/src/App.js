e5e6698bf8e69aa4f0aa5660f604435651901756
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-01-15 13:46:06 -0500

    added profile page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
 function App() {
   return (
@@ -9,8 +17,9 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/profile" component={Profile} />
         </Switch>
-            </div>
+      </div>
     </Router>
 
   );
