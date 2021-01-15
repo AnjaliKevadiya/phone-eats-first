da85b9fbc2ca4104aebb0a39d7512fde41073caf
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-15 11:36:45 -0500

    get the pots data on home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PostCard from "../components/PostCard";
 import "./style.css";
 import NewPostButton from "../components/NewPostButton";
+import API from "../utils/API";
 
 function Home() {
+  // Setting our component's initial state
+  const [posts, setPosts] = useState([]);
+
+  // Load all posts and store them with setPosts
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  // Loads all posts and sets them to pots
+  function loadPosts() {
+    API.getAllPosts()
+      .then((res) => {
+        console.log("posts", res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div>
       <NewPostButton />
-      <div className="postCard">
-        <PostCard />
-      </div>
-      <div className="postCard">
-        <PostCard />
-      </div>
-      <div className="postCard">
-        <PostCard />
-      </div>
+      {posts.length ? (
+        <div className="postCard">
+          <PostCard />
+        </div>
+      ) : (
+        <h3>No Results to Display</h3>
+      )}
     </div>
   );
 }
