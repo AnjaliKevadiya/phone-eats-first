e82b3c2d087deb2143b9df6e5abac7817066f2a6
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-16 20:47:38 -0500

    api call to upload new post using static data

--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -1,5 +1,25 @@
 import React from "react";
 import "./style.css";
+import API from "../utils/API";
+import { Button } from "@material-ui/core";
+
+const handleUploadImageClick = (e) => {
+  e.preventDefault();
+
+  API.createNewPost({
+    image: "url",
+    restaurant_name: "Mexican",
+    rating: 3,
+    caption: "Tasty!!",
+    number_of_likes: 0,
+    username: "Anji",
+  })
+    .then((res) => {
+      console.log("post created ", res);
+      window.location.href = "/home";
+    })
+    .catch((err) => console.log(err));
+};
 
 function NewPost() {
   return <FileUpload />;
@@ -25,6 +45,14 @@ function FileUpload() {
 
   return (
     <div id="upload-box">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleUploadImageClick}
+      >
+        Upload Image
+      </Button>
+
       <input type="file" accept="image/*" onChange={handleUpload} />
       <p>Filename: {file.name}</p>
       <p>File type: {file.type}</p>
