ddc1679a4daf2ea5c729c8c0bc4ee7f87d1c315d
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-18 12:57:11 -0500

    set rounded corner button for facebook

--- client/src/components/FBShareButton/index.js
+++ client/src/components/FBShareButton/index.js
@@ -1,30 +1,14 @@
 import React from "react";
 import { FacebookShareButton, FacebookIcon } from "react-share";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    borderRadius: "2px",
-  },
-  //   socialMediaButton: {
-  //     "&:hover > svg": {
-  // height: "50px !important",
-  // width: "50px !important",
-  //     },
-  //   },
-}));
 
 export default function FBShareButton(props) {
-  const classes = useStyles();
-
   return (
     <FacebookShareButton
       url={"https://phone-eats-first.herokuapp.com/"}
       quote={"Phone Eats First"}
       //   hashtag="#Testing"
-      // className={classes.root}
     >
-      <FacebookIcon size={36} />
+      <FacebookIcon size={36} round={true} />
     </FacebookShareButton>
   );
 }
