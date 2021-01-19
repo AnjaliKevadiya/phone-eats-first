452b4603140b1c7272b805c8ff331aa62003ba1a
joedip16 <joedippolito16@gmail.com>
2021-01-19 11:55:27 -0500

    added facebook and google login methods to sign in page

--- client/src/pages/signIn.js
+++ client/src/pages/signIn.js
@@ -3,9 +3,43 @@ import { Container, Row } from "../components/SignUpForm";
 import API from "../utils/API";
 import "./style.css"
 import EntryNavBar from "../components/EntryNavBar"
+import { GoogleLogin } from "react-google-login";
+import FacebookLogin from "react-facebook-login";
 
 
 function SignIn() {
+    //GOOGLE LOGIN SUCCESS
+    const responseSuccessGoogle = (res) => {
+        console.log("Google Success: ", res);
+        API.googlelogin({
+            tokenId: res.tokenId
+        })
+            .then((res) => {
+                console.log("api.googlelogin -res: ", res)
+                window.location.href = "/home";
+            })
+            .catch((err) => console.log("api.googlelogin error: ", err));
+    }
+
+    //GOOGLE LOGIN ERROR
+    const responseErrorGoogle = (res) => {
+        console.log("Google Login Error: ", res);
+    }
+
+    //FACEBOOK LOGIN SUCCESS
+    const responseFacebook = (res) => {
+        console.log("Facebook Login: ", res);
+        API.facebooklogin({
+            accessToken: res.accessToken,
+            userID: res.userID
+        })
+            .then((res) => {
+                console.log("api.facebooklogin - res: ", res)
+                window.location.href = "/home";
+            })
+            .catch((err) => console.log("api.facebooklogin error: ", err));
+    }
+
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const handleSubmit = (e) => {
@@ -51,8 +85,21 @@ function SignIn() {
                                 />
                             </Row>
                             <button className="btn btn-success" type="submit">
-                                Submit
+                                Sign In
                             </button>
+                            <GoogleLogin className="googleLogin"
+                                clientId="1082885186579-00j5a8kbt4tt0q3h6mua0b1ei0fgu9n1.apps.googleusercontent.com"
+                                buttonText="Continue with Google"
+                                onSuccess={responseSuccessGoogle}
+                                onFailure={responseErrorGoogle}
+                                cookiePolicy={'single_host_origin'}
+                            />
+                            <FacebookLogin className="facebookLogin"
+                                appId="3217579161677338"
+                                autoLoad={false}
+                                callback={responseFacebook}
+                            />
+
                             <p className="signUp">To sign up click <a href="/signup">here</a></p>
                         </Container>
                     </form>
